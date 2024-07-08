5a3090063db2ed11ccbcb4f9fa8e3545d193ae4a
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-07-08 18:37:57 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+// src/components/Header.tsx
+import React from "react";
+
+const Header: React.FC = () => {
+   return (
+      <header className="bg-peach p-4 mt-0">
+         <h1 className="text-white text-2xl">My Recipe App</h1>
+      </header>
+   );
+};
+
+export default Header;
